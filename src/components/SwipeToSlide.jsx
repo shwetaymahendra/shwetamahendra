cb477d45d5161338fdc1bf0935cdbab2e4ee1c90
Deepak Chandra MAurya <44018172+deepakchandra4@users.noreply.github.com>
2023-05-01 22:38:02 +0530

    ...

--- src/components/SwipeToSlide.jsx
+++ src/components/SwipeToSlide.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
-
+import {MdOutlineKeyboardArrowRight} from 'react-icons/md';
 
 export default class SwipeToSlide extends Component {
   render() {
@@ -41,38 +41,15 @@ export default class SwipeToSlide extends Component {
     };
     return (
       <div className="max-h-lg py-10 ">
-        <a href="https://tailwindcss.com/docs/align-content">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6 cursor-pointer hover:fill-purple-500 "
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-        </a>
-        <a href="https://tailwindcss.com/docs/align-content">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6 cursor-pointer  hover:fill-purple-500"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-        </a>
+         <div className="pr-6">
+            <a href="https://tailwindcss.com/docs/align-content" target="_blank" rel="noreferrer" smooth duration={500} className=" group text-white w-fit md:max-w-96 px-4 py-2 my-2 flex rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 
+			cursor-pointer text-right ml-auto ">
+              View more
+              <span className="group-hover:scale-110 duration-300">
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1"/>
+              </span>
+            </a>
+          </div>
 
         <Slider {...settings}>
           <div className="p-4">
