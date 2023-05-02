47e27693bdaee6e6855e398d49c1cc8fbad143ef
Deepak Chandra MAurya <44018172+deepakchandra4@users.noreply.github.com>
2023-05-02 19:49:58 +0530

    ..

--- src/components/SwipeToSlide.jsx
+++ src/components/SwipeToSlide.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
-import {MdOutlineKeyboardArrowRight} from 'react-icons/md';
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 export default class SwipeToSlide extends Component {
   render() {
@@ -13,43 +13,48 @@ export default class SwipeToSlide extends Component {
       swipeToSlide: true,
       responsive: [
         {
-          breakpoint : 1280,
-          settings : {
-            slidesToShow:4,
-          }
+          breakpoint: 1280,
+          settings: {
+            slidesToShow: 4,
+          },
         },
         {
-          breakpoint : 1000,
-          settings : {
-           slidesToShow:3,
-          }
+          breakpoint: 1000,
+          settings: {
+            slidesToShow: 3,
+          },
         },
         {
-          breakpoint : 650,
-          settings : {
-            slidesToShow:2,
-          }
-        }
+          breakpoint: 650,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
       ],
       afterChange: function (index) {
         console.log(
           `Slider Changed to: ${index + 1}, background: #222; color: #bada55`
         );
       },
-
-
     };
     return (
       <div className="max-h-lg py-10 ">
-         <div className="pr-6">
-            <a href="https://tailwindcss.com/docs/align-content" target="_blank" rel="noreferrer" smooth duration={500} className=" group text-white w-fit md:max-w-96 px-4 py-2 my-2 flex rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 
-			cursor-pointer text-right ml-auto ">
-              View more
-              <span className="group-hover:scale-110 duration-300">
-                <MdOutlineKeyboardArrowRight size={25} className="ml-1"/>
-              </span>
-            </a>
-          </div>
+        <div className="pr-6">
+          <a
+            href="https://voluble-hotteok-c43fbc.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+            smooth
+            duration={500}
+            className=" group text-white w-fit md:max-w-96 py-2 my-2 flex rounded-md  
+			cursor-pointer text-right ml-auto hover:scale-105 duration-300 "
+          >
+            View more
+            <span className="group-hover:scale-110 duration-300">
+              <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+            </span>
+          </a>
+        </div>
 
         <Slider {...settings}>
           <div className="p-4">
