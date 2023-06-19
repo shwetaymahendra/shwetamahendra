4a9c514f550876da696012200c5c0420b6cb692d
Deepak Chandra MAurya <44018172+deepakchandra4@users.noreply.github.com>
2023-06-19 19:32:19 +0530

    ...

--- src/components/SwipeToSlideNews.jsx
+++ src/components/SwipeToSlideNews.jsx
@@ -83,16 +83,36 @@ export default class SwipeToSlide extends Component {
             />
             </a>
           </div>
-          {/* <div className="p-4">
-            <a href="https://epaper.heavenmail.in/epaper/edition/648/heavenmail-april/page/5" target="_blank" rel="noreferrer">
+          <div className="p-4">
+            <a href="https://www.sudarshannews.in/nd.aspx?id=93237" target="_blank" rel="noreferrer">
             <img
-              src={HeavenMailReiview3}
+              src="https://assets-news-bcdn.dailyhunt.in/cmd/resize/1066x1600_90/fetchdata16/images/50/fd/2e/50fd2e30e1257c416b6cb925705458cbe859a82744cc07fed927fc03f1791a20.webp"
               alt=""
               className="rounded-2xl"
             />
             </a>
           </div>
-           */}
+          
+          <div className="p-4">
+            <a href="https://www.joshbharat.com/2023/06/ShwetaMahendrabooks.html" target="_blank" rel="noreferrer">
+            <img
+              src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgNdeK9JSDOzFBqqMx8G2oSmbUleMFm2XV9uUU9JZCrIEXsQ0Sv7jPuUr-U9ptV65Z4qsgA1neHn9-nLCHNmrKHvrA8SA7Rx6Am2iikX6VFgYpoWGTEbwQW6kmE3zCEIiBLPWTXOZuok8W6ogWs_LRHvjOvNUn0AcHgDCIS9h3byaRoUMp7ICHKuw/s1600/IMG-20230610-WA0247(1).jpg"
+              alt=""
+              className="rounded-2xl"
+            />
+            </a>
+          </div>
+          
+          <div className="p-4">
+            <a href="https://m.dailyhunt.in/news/india/hindi/josh+bharat-epaper-dh832812b9d1ff451aa43380d9b51fa20e/ek+adabhut+pratibha+ki+dhani+shveta+mahindra+ne+likhi+pustak-newsid-dh832812b9d1ff451aa43380d9b51fa20e_7f61228007a311eebdf564a61a1879c8?sm=Y" target="_blank" rel="noreferrer">
+            <img
+              src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgNdeK9JSDOzFBqqMx8G2oSmbUleMFm2XV9uUU9JZCrIEXsQ0Sv7jPuUr-U9ptV65Z4qsgA1neHn9-nLCHNmrKHvrA8SA7Rx6Am2iikX6VFgYpoWGTEbwQW6kmE3zCEIiBLPWTXOZuok8W6ogWs_LRHvjOvNUn0AcHgDCIS9h3byaRoUMp7ICHKuw/s1600/IMG-20230610-WA0247(1).jpg"
+              alt=""
+              className="rounded-2xl"
+            />
+            </a>
+          </div>
+          
         </Slider>
       </div>
     );
